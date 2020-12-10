65d9ec5d2ac6d7e4d55a6eb4c117cd847eea12bf
Mitchell Gritts <mitchellgritts@pm.me>
2020-12-09 18:00:04 -0800

    Update configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.{vue,js}'
+  ],
   darkMode: false,
   theme: {
     extend: {
